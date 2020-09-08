2801dcaf932598d55b2028b68ba4cf39adeb75f0
Subramanian <s35subra@edu.uwaterloo.ca>
2020-09-08 06:30:16 -0400

    Updates

---
+++ precache-manifest.ebcdc27f8bebcdddb4bab2d417dfa0b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2807abc1022ec49aea5f8850bdd5b10e",
+    "url": "/shopify-challenge/index.html"
+  },
+  {
+    "revision": "a45cbad2c788b7abaef0",
+    "url": "/shopify-challenge/static/css/main.a170ffac.chunk.css"
+  },
+  {
+    "revision": "b942b76cd8a62e62b5e0",
+    "url": "/shopify-challenge/static/js/2.f0741033.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shopify-challenge/static/js/2.f0741033.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45cbad2c788b7abaef0",
+    "url": "/shopify-challenge/static/js/main.b55e3804.chunk.js"
+  },
+  {
+    "revision": "df37baafe4333b7dda7f",
+    "url": "/shopify-challenge/static/js/runtime-main.3b7cb11a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shoppies/precache-manifest.2de607ec968184101db52e93d2540d05.js"
+  "/shopify-challenge/precache-manifest.ebcdc27f8bebcdddb4bab2d417dfa0b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shoppies/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shopify-challenge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
